c6ba8ba1e97fcf9a230c748e846baa502153428c
Pedro Pinto <phgoncalves2603@gmail.com>
2025-10-16 22:12:19 -0400

    first Commit

---
+++ src/main/java/com/LearningDocker/Learning/Docker/LearningDockerApplication.java
@@ -0,0 +1,13 @@
+package com.LearningDocker.Learning.Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LearningDocker/Learning/Docker/LearningDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearningDocker.Learning.Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
